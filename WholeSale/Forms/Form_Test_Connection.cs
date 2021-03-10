50360f595bd16de1141c2150141f200543922bc6
SATIT PONGPIMOL <150-AIAP@ap01.aisingroup.com>
2021-03-10 14:43:52 +0700

    new add form test connection

---
+++ WholeSale/Forms/Form_Test_Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WholeSale.Forms
+{
+    public partial class Form_Test_Connection : Form
+    {
+        public Form_Test_Connection()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Test_Connection_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, Conection");
+        }
+    }
+}
